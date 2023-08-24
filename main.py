38b8ddab1495dcd5c119961c610a38b37026ffef
thanakorn35938 <983081-thanakorn35938@users.noreply.replit.com>
2023-08-24 07:08:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+w = tk.Tk()
+w.title("SomeOne")
+w.config(bg='navy')
+w.geometry('250x200')
+w.resizable(0,0)
+l1=tk.Label(text="profie",bg='pink',fg='blue').pack()
+m1=tk.Message(text='My name is Thanakorn. I am ACC finalist speacilize in visual processing. I study at SK. I am generation Sk145-GATE Program').pack()
